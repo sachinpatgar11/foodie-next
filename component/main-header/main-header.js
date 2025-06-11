6787b505fdae59ffd7907e726acf7619bb80268c
sachinpatgar11 <sachinpatgar39@gmail.com>
2025-06-11 20:49:18 +0530

    updated header

---
+++ component/main-header/main-header.js
@@ -0,0 +1,31 @@
+import { headers } from "next/headers";
+import Link from "next/link";
+import Image from "next/image";
+
+import LogoImg from "@/assets/logo.png";
+import classes from "./main-header.module.css";
+import MainHeaderBackground from "./main-header-background";
+
+export default function MainHeader() {
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+          <Image src={LogoImg} alt="Main Logo" priority />
+          NextLevel Food
+        </Link>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href="/melas">Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
