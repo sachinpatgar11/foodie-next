573ee08c76944729ae76b5b425e5f0207868b588
sachinpatgar11 <sachinpatgar39@gmail.com>
2025-06-13 07:40:47 +0530

    adding image slider

--- app/page.js
+++ app/page.js
@@ -1,11 +1,14 @@
 import Link from "next/link";
 import classes from "./page.module.css";
+import ImageSlideshow from "@/component/images/image-slideshow";
 
 export default function Home() {
   return (
     <>
       <header className={classes.header}>
-        <div className={classes.slideShow}></div>
+        <div className={classes.slideshow}>
+          <ImageSlideshow />
+        </div>
 
         <div>
           <div className={classes.hero}>
