7584e63ed3c6aab5fe12f6cc9f96e3201a920757
sachinpatgar11 <sachinpatgar39@gmail.com>
2025-06-13 08:11:13 +0530

    active navigation link

--- component/main-header/main-header.js
+++ component/main-header/main-header.js
@@ -1,10 +1,10 @@
-import { headers } from "next/headers";
 import Link from "next/link";
 import Image from "next/image";
 
 import LogoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
 import MainHeaderBackground from "./main-header-background";
+import NavLink from "./nav-link";
 
 export default function MainHeader() {
   return (
@@ -18,10 +18,10 @@ export default function MainHeader() {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/melas">Meals</Link>
+              <NavLink href="/meals">Meals</NavLink>
             </li>
             <li>
-              <Link href="/community">Community</Link>
+              <NavLink href="/community">Community</NavLink>
             </li>
           </ul>
         </nav>
