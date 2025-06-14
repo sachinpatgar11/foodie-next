6e4566809eac3d1f41940318620284ba82ca46cc
sachinpatgar11 <sachinpatgar39@gmail.com>
2025-06-14 12:05:34 +0530

    meal items added

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,24 @@
+import Link from "next/link";
+
+import MealsGrid from '@/component/meals/meal-grid.js'
+import classes from "./page.module.css";
+
 export default function MealsPage() {
   return (
     <>
-      <h1>Meals Page</h1>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals created {""}{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>Choose your favorite recipe and cook it yourself</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share"> Share your recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid />
+      </main>
     </>
   );
 }
---
+++ component/meals/meals-grid.js
@@ -0,0 +1,14 @@
+
+import MealItem from './meal-item.js'
+import claases from './meal-grid.module.css'
+
+export default function MealsGrid({meals}){
+return(
+    <ul>
+        {meals.map( meal => <li key={meal.id}>
+            <MealItem {...meal}/>
+        </li>)}
+    </ul>
+)
+
+}
